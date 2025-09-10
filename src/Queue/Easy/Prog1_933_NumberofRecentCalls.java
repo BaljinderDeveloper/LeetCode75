3fa9660c93424b2b6bb734659521fa612c48a2d7
Baljinder Singh <baljindersingh@Baljinders-MacBook-Air.local>
2025-09-10 12:42:14 +0530

    commit changes in project name

---
+++ src/Queue/Easy/Prog1_933_NumberofRecentCalls.java
@@ -0,0 +1,17 @@
+package Queue.Easy;
+
+public class Prog1_933_NumberofRecentCalls {
+    public static void main(String[] args) {
+
+        StringBuffer s1 = new StringBuffer();
+        StringBuilder s2 = new StringBuilder();
+
+    }
+//    public RecentCounter() {
+//
+//    }
+//
+//    public int ping(int t) {
+//
+//    }
+}
