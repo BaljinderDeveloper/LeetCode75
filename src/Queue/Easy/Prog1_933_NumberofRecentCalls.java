de8f806e30fbb676d786fb64482b3d6f2dfdf177
Baljinder Singh <baljindersingh@Baljinders-MacBook-Air.local>
2025-09-11 13:46:14 +0530

    Queue problem added

--- src/Queue/Easy/Prog1_933_NumberofRecentCalls.java
+++ src/Queue/Easy/Prog1_933_NumberofRecentCalls.java
@@ -1,17 +1,26 @@
 package Queue.Easy;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Prog1_933_NumberofRecentCalls {
     public static void main(String[] args) {
-
-        StringBuffer s1 = new StringBuffer();
-        StringBuilder s2 = new StringBuilder();
-
+        Prog1_933_NumberofRecentCalls obj = new Prog1_933_NumberofRecentCalls();
+        obj.ping(1);
+        obj.ping(100);
+        obj.ping(3001);
+        System.out.println(obj.ping(3002));
     }
-//    public RecentCounter() {
-//
-//    }
-//
-//    public int ping(int t) {
-//
-//    }
+    Queue<Integer> que;
+    public Prog1_933_NumberofRecentCalls() {
+        que = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        que.add(t);
+        while(que.peek() < t-3000){
+            que.poll();
+        }
+        return que.size();
+    }
 }
